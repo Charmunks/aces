96c7a8d3e369f43827f0ef3b9c6bef9712749b91
Niko <137782638+thesleepyniko@users.noreply.github.com>
2025-10-15 18:38:26 -0400

    Revert "Change file structure, add python backend"
    This reverts commit 7802094f2cf31daf4890730c0a77a7cee423e355.

--- script.js
+++ script.js
@@ -15,34 +15,22 @@ const modalContent = {
 };
 let hasReferral = Boolean(params.get("ref")); // checks if theres a referral
 let url; // define first so we can access later
-let valid = false;
 
-if (hasReferral && isNaN(Number(params.get("ref")))) {
-    hasReferral = false;
-}
-
+// does it have a referral? if yes -> put the ref parameter in the form
 if (hasReferral) {
-(async () => { // this whole chunk is to validate it with our backend
-await fetch(`/api/referral?referral=${params.get("ref")}`)
-  .then((r) => r.json())
-  .then((data) => {
-	console.log(data)
-    if (!("valid" in data)) { throw new Error(); }
-	if (data.valid) {
-		valid = true
-	}
-    else {
-		valid = false
-	}
-  })
-  .catch(console.error)
-  if (hasReferral && valid) {
 	url = `https://forms.hackclub.com/aces-rsvp?ref=${params.get("ref")}`
-  }
-  // otherwise, set the raw link and do nothing
-  else {
+}
+// otherwise, set the raw link and do nothing
+else {
 	url = "https://forms.hackclub.com/aces-rsvp"
 }
+
+modalExit.addEventListener("click", function(event) {
+	modal.classList.remove("active");
+	body.style.overflow = "auto";
+})
+
+// replace all links with the current url constructed above
 cards.forEach(card => {
 	card.setAttribute("data-link", url);
 	card.addEventListener("click", function(event) {
@@ -53,28 +41,6 @@ cards.forEach(card => {
 		body.style.overflow = "hidden";
 	})
 })
-}
-
-)()};
-
-// const valid = (async() => {await fetch(`/api/referral?referral=${refCode}`)
-//   .then((r) => r.json())
-//   .then((data) => {
-// 	console.log(data)
-//     if (!("valid" in data)) { throw new Error(); }
-//     return data.valid
-//   })
-//   .catch(console.error)})
-
-// does it have a referral and is it a valid referral? if yes -> put the ref parameter in the form
-
-
-modalExit.addEventListener("click", function(event) {
-	modal.classList.remove("active");
-	body.style.overflow = "auto";
-})
-
-// replace all links with the current url constructed above
 
 // Fetch and display RSVP count
 const rsvpEl = document.getElementById('rsvp-count');
